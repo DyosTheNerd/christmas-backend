ee80566035c8d0a1523fe083f4d9bf759bd90e4d
JDWTheNerdAutomaton <thenerdautomaton@gmail.com>
2021-04-16 12:39:11 +0900

    updating dependencies

--- src/test/java/de/the/nerd/automaton/christmas/backend/ApiDemoApplicationTests.java
+++ src/test/java/de/the/nerd/automaton/christmas/backend/ApiDemoApplicationTests.java
@@ -1,16 +1,25 @@
 package de.the.nerd.automaton.christmas.backend;
 
-import org.junit.Test;
+
+import com.google.common.base.Preconditions;
+import org.junit.jupiter.api.Test;
 import org.junit.runner.RunWith;
 import org.springframework.boot.test.context.SpringBootTest;
 import org.springframework.test.context.junit4.SpringRunner;
 
+
+import static org.junit.jupiter.api.Assertions.assertEquals;
+
+
 @RunWith(SpringRunner.class)
 @SpringBootTest
+
 public class ApiDemoApplicationTests {
 
 	@Test
 	public void contextLoads() {
+
+		assertEquals(true,true);
 	}
 
 }
